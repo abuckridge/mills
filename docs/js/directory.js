182ce4806fdb8197183471458e2387ea7d8a2651
abuckridge <49199167+abuckridge@users.noreply.github.com>
2019-10-08 10:21:05 -0700

    Update directory.js

--- docs/js/directory.js
+++ docs/js/directory.js
@@ -12,7 +12,7 @@ function getPages(callback) {
 
     //https://docs.google.com/spreadsheets/d/1x-7ms5fDNzW2Ah9AXylV0ERWh4O-YlwVpguptmWlWbU/edit?usp=sharing#gid=0;
     var sheetURL =
-    "https://docs.google.com/spreadsheets/d/1oE4XyuSEh1J-eEy44AoWUNk3fd_1k48AlEOTrII_pK0/edit?usp=sharing"
+    "https://docs.google.com/spreadsheets/d/1oE4XyuSEh1J-eEy44AoWUNk3fd_1k48AlEOTrII_pK0/edit?usp=sharing#gid=0"
     var pages = [];
     $('#sheetrock').sheetrock({
         url: sheetURL,
